7d43062304aa362e3d923e47784d42cfefdff2be
Shelby-Simpson <37957895+Shelby-Simpson@users.noreply.github.com>
2018-05-03 00:19:33 -0400

    Add files via upload

--- create.sql
+++ create.sql
@@ -8,7 +8,7 @@
 CREATE TABLE Part (
 PartID INTEGER NOT NULL PRIMARY KEY CHECK (PartID > 0),
 Name VARCHAR(30) NOT NULL CHECK (Name <> ' '),
-UnitCost FLOAT NOT NULL CHECK(UnitCost>0);
+UnitCost FLOAT NOT NULL CHECK (UnitCost>0));
 
 CREATE TABLE Customer (
 CustomerID INTEGER NOT NULL PRIMARY KEY CHECK (CustomerID > 0),
@@ -16,7 +16,7 @@ Name VARCHAR(30) NOT NULL CHECK (Name <> ' '),
 Street VARCHAR(30) NOT NULL CHECK (Street <> ' '),
 City VARCHAR(20) NOT NULL CHECK (City <> ' '),
 State CHAR(2) NOT NULL CHECK (State <> ' '),
-Zip VARCHAR(10) NOT NULL CHECK (Zip <> ' ');
+Zip VARCHAR(10) NOT NULL CHECK (Zip <> ' '));
 
 CREATE TABLE Technician (
 TechnicianID INTEGER NOT NULL PRIMARY KEY CHECK (TechnicianID > 0),
@@ -24,7 +24,7 @@ Name VARCHAR(30) NOT NULL CHECK (Name <> ' '),
 Street VARCHAR(30) CHECK (Street <> ' '),
 City VARCHAR(20) CHECK (City <> ' '),
 State CHAR(2) CHECK (State <> ' '),
-Zip VARCHAR(10) CHECK (Zip <> ' ');
+Zip VARCHAR(10) CHECK (Zip <> ' '));
 
 CREATE TABLE Supplier (
 SupplierID INTEGER NOT NULL PRIMARY KEY CHECK (SupplierID > 0),
@@ -32,89 +32,69 @@ Name VARCHAR(30) NOT NULL CHECK (Name <> ' '),
 Street VARCHAR(30) NOT NULL CHECK (Street <> ' '),
 City VARCHAR(20) NOT NULL CHECK (City <> ' '),
 State CHAR(2) NOT NULL CHECK (State <> ' '),
-Zip VARCHAR(10) NOT NULL CHECK (Zip <> ' ');
+Zip VARCHAR(10) NOT NULL CHECK (Zip <> ' '));
 
 CREATE TABLE COrder_t (
 OrderID INTEGER NOT NULL PRIMARY KEY CHECK (OrderID > 0),
-DateToFill DATE,
-CustomerID INTEGER NOT NULL FOREIGN KEY REFERENCES Customer CHECK (CustomerID > 0),
-PartID INTEGER NOT NULL FOREIGN KEY REFERENCES Part CHECK (PartID > 0);
+DateToFill DATE CHECK (DateToFill >= CURRENT_DATE),
+CustomerID INTEGER NOT NULL REFERENCES Customer CHECK (CustomerID > 0),
+PartID INTEGER NOT NULL REFERENCES Part CHECK (PartID > 0));
 
 CREATE TABLE AssemblyCard (
 CardNumber INTEGER NOT NULL PRIMARY KEY CHECK (CardNumber > 0),
-OrderID INTEGER NOT NULL FOREIGN KEY REFERENCES COrder_t CHECK (OrderID > 0);
+OrderID INTEGER NOT NULL REFERENCES COrder_t CHECK (OrderID > 0));
 
 CREATE TABLE SupplyShipment (
-SupplyID INTEGER NOT NULL CHECK (SupplyID > 0),
+SupplyID INTEGER NOT NULL PRIMARY KEY CHECK (SupplyID > 0),
 Quantity INTEGER NOT NULL CHECK (Quantity > 0),
-SupplierID INTEGER NOT NULL CHECK (SupplierID > 0),
-PartID INTEGER NOT NULL CHECK PartID > 0),
-CONSTRAINT supply_shipment_pk PRIMARY KEY (SupplyID),
-CONSTRAINT supply_shipment_supplier_fk FOREIGN KEY (SupplierID) REFERENCES Supplier,
-CONSTRAINT supply_shipment_part_fk FOREIGN KEY (PartID) REFERENCES Part);
+SupplierID INTEGER NOT NULL REFERENCES Supplier CHECK (SupplierID > 0),
+PartID INTEGER NOT NULL REFERENCES Part CHECK (PartID > 0));
 
 CREATE TABLE Assembles (
-TechnicianID INTEGER NOT NULL CHECK (TechnicianID > 0),
-PartID INTEGER NOT NULL CHECK (PartID > 0),
-CONSTRAINT assembles_pk PRIMARY KEY (TechnicianID, PartID),
-CONSTRAINT assembles_technician_fk FOREIGN KEY (TechnicianID) REFERENCES Technician,
-CONSTRAINT assembles_part_fk FOREIGN KEY (PartID) REFERENCES Part);
+TechnicianID INTEGER NOT NULL REFERENCES Technician CHECK (TechnicianID > 0),
+PartID INTEGER NOT NULL REFERENCES Part CHECK (PartID > 0),
+CONSTRAINT assembles_pk PRIMARY KEY (TechnicianID, PartID));
 
 CREATE TABLE Ships (
-SupplierID INTEGER NOT NULL CHECK (SupplierID > 0),
-SupplyID INTEGER NOT NULL CHECK (SupplyID > 0),
-CONSTRAINT ships_pk PRIMARY KEY (SupplierID, SupplyID),
-CONSTRAINT ships_supplier_fk FOREIGN KEY (SupplierID) REFERENCES Supplier,
-CONSTRAINT ships_supply_fk FOREIGN KEY (SupplyID) REFERENCES SupplyShipment);
+SupplierID INTEGER NOT NULL REFERENCES Supplier CHECK (SupplierID > 0),
+SupplyID INTEGER NOT NULL REFERENCES SupplyShipment CHECK (SupplyID > 0),
+CONSTRAINT ships_pk PRIMARY KEY (SupplierID, SupplyID));
 
 CREATE TABLE Contains (
-PartID INTEGER NOT NULL CHECK (PartID > 0),
-SupplyID INTEGER NOT NULL CHECK (SupplyID > 0),
-CONSTRAINT contains_pk PRIMARY KEY (PartID, SupplyID),
-CONSTRAINT contains_part_fk FOREIGN KEY (PartID) REFERENCES Part,
-CONSTRAINT contains_supply_fk FOREIGN KEY (SupplyID) REFERENCES SupplyShipment);
+PartID INTEGER NOT NULL REFERENCES Part CHECK (PartID > 0),
+SupplyID INTEGER NOT NULL REFERENCES SupplyShipment CHECK (SupplyID > 0),
+CONSTRAINT contains_pk PRIMARY KEY (PartID, SupplyID));
 
 CREATE TABLE BOM (
-CompositePart INTEGER CHECK (CompositePart > 0),
-ComponentPart INTEGER CHECK (ComponentPart > 0),
-CONSTRAINT bom_pk PRIMARY KEY (CompositePart, ComponentPart),
-CONSTRAINT bom_composite_fk FOREIGN KEY (CompositePart) REFERENCES Part,
-CONSTRAINT bom_component_fk FOREIGN KEY (ComponentPart) REFERENCES Part);
+CompositePart INTEGER REFERENCES Part CHECK (CompositePart > 0),
+ComponentPart INTEGER REFERENCES Part CHECK (ComponentPart > 0),
+CONSTRAINT bom_pk PRIMARY KEY (CompositePart, ComponentPart));
 
 CREATE TABLE Uses (
-TechnicianID INTEGER NOT NULL CHECK (TechnicianID > 0),
-OrderID INTEGER NOT NULL CHECK (OrderID > 0),
-CONSTRAINT use_pk PRIMARY KEY (TechnicianID, OrderID),
-CONSTRAINT uses_technician_fk FOREIGN KEY (TechnicianID) REFERENCES Technician,
-CONSTRAINT uses_order_fk FOREIGN KEY (OrderID) REFERENCES COrder_t);
+TechnicianID INTEGER NOT NULL REFERENCES Technician CHECK (TechnicianID > 0),
+CardNumber INTEGER NOT NULL REFERENCES AssemblyCard CHECK (CardNumber > 0),
+CONSTRAINT use_pk PRIMARY KEY (TechnicianID, CardNumber));
 
 CREATE TABLE ForPart_t (
-PartID INTEGER NOT NULL CHECK (PartID > 0),
-OrderID INTEGER NOT NULL CHECK (OrderID > 0),
-Quantity INTEGER NOT NULL,
-CHECK (Quantity >= 0),
-CONSTRAINT for_pk PRIMARY KEY (PartID, OrderID),
-CONSTRAINT for_part_fk FOREIGN KEY (PartID) REFERENCES Part,
-CONSTRAINT for_order_fk FOREIGN KEY (OrderID) REFERENCES COrder_t);
+PartID INTEGER NOT NULL REFERENCES Part CHECK (PartID > 0),
+OrderID INTEGER NOT NULL REFERENCES COrder_t CHECK (OrderID > 0),
+Quantity INTEGER NOT NULL CHECK (Quantity >= 0),
+CONSTRAINT for_pk PRIMARY KEY (PartID, OrderID));
 
 CREATE VIEW ForPart AS
-SELECT PartID, OrderID, Quantity, (UnitCost * Quantity) AS LineTotal CHECK (LineTotal >= 0)
+SELECT PartID, OrderID, Quantity, (UnitCost * Quantity) AS LineTotal
 FROM ForPart_t NATURAL JOIN Part;
 
 CREATE VIEW COrder AS
-SELECT OrderID, (SELECT SUM(LineTotal) FROM ForPart) AS OrderTotal CHECK (OrderTotal >= 0)
+SELECT OrderID, (SELECT SUM(LineTotal) FROM ForPart) AS OrderTotal
 FROM COrder_t;
 
 CREATE TABLE Places (
-CustomerID INTEGER NOT NULL CHECK (CustomerID > 0),
-OrderID INTEGER NOT NULL CHECK OrderID > 0),
-CONSTRAINT places_pk PRIMARY KEY (CustomerID, OrderID),
-CONSTRAINT places_customer_fk FOREIGN KEY (CustomerID) REFERENCES Customer,
-CONSTRAINT places_order_fk FOREIGN KEY (OrderID) REFERENCES COrder_t);
+CustomerID INTEGER NOT NULL REFERENCES Customer CHECK (CustomerID > 0),
+OrderID INTEGER NOT NULL REFERENCES COrder_t CHECK (OrderID > 0),
+CONSTRAINT places_pk PRIMARY KEY (CustomerID, OrderID));
 
 CREATE TABLE Creates (
-OrderID INTEGER NOT NULL CHECK (OrderID > 0),
-CardNumber INTEGER NOT NULL CHECK (CardNumber > 0),
-CONsTRAINT creates_pk PRIMARY KEY (OrderID, CardNumber),
-CONSTRAINT creates_order_fk FOREIGN KEY (OrderID) REFERENCES COrder_t,
-CONSTRAINT creates_assemblycard_fk FOREIGN KEY (CardNumber) REFERENCES AssemblyCard);
+OrderID INTEGER NOT NULL REFERENCES COrder_t CHECK (OrderID > 0),
+CardNumber INTEGER NOT NULL REFERENCES AssemblyCard CHECK (CardNumber > 0),
+CONsTRAINT creates_pk PRIMARY KEY (OrderID, CardNumber));
--- userEdits.sql
+++ userEdits.sql
@@ -9,7 +9,22 @@
 INSERT INTO Part
 VALUES (0001, 'Turbine', 50000),
 (0002, 'Wing', 25000),
-(0003, 'Fuselage', 80000);
+(0003, 'Fuselage', 80000),
+(0004, 'Flaps', 5000),
+(0005, 'Aileron', 20000);
+
+/*Creating BOM relationships*/
+INSERT INTO BOM
+VALUES (0002, 0004),
+(0002, 0005);
+
+/* Hiring a technician */
+INSERT INTO Technician
+VALUES (0001, 'Elon Musk', NULL);
+
+/*Creating an Assembles relationship*/
+INSERT INTO Assembles
+VALUES (0001, 0002);
 
 /* Inserting customers */
 INSERT INTO Customer
@@ -28,18 +43,37 @@ WHERE Name='Turbine';
 
 /* Creating orders */
 INSERT INTO COrder_t
-VALUES (0000001, 00001, 0002),
-(0000002, 00002, 0003);
+VALUES (0000001, '2020-06-09', 00001, 0002),
+(0000002, '2019-09-02', 00002, 0003);
 
 /* Creating Assembly Cards for the orders */
 INSERT INTO AssemblyCard
-VALUES (0000001, 0000001, NULL),
-(0000002, 0000002, '2018-09-23');
+VALUES (0000001, 0000001),
+(0000002, 0000002);
 
-/* Editing an exact date */
-UPDATE AssemblyCard
-SET ExactDate='2018-12-12'
-WHERE CardNumber=0000001;
+/* Creating creates relationships */
+INSERT INTO Creates
+VALUES (0000001, 0000001),
+(0000002, 0000002);
+
+/* Creating places relationships */
+INSERT INTO Places
+VALUES (00001, 0000001),
+(00002, 0000002);
+
+/* Creating ForPart_t relationships */
+INSERT INTO ForPart_t
+VALUES (0002, 0000001, 3),
+(0003, 0000002, 1);
+
+/* Editing a date to fill */
+UPDATE COrder_t
+SET DateToFill='2018-12-12'
+WHERE OrderID=0000001;
+
+/* Creating a Uses relationship */
+INSERT INTO Uses
+VALUES (0001, 0000001);
 
 /* Adding suppliers */
 INSERT INTO Supplier
@@ -51,13 +85,35 @@ UPDATE Supplier
 SET Zip=85010
 WHERE SupplierID=00002;
 
-/* Hiring a technician */
-INSERT INTO Technician
-VALUES (0001, 'Elon Musk', NULL);
+/* Requesting a supply shipment */
+INSERT INTO SupplyShipment
+VALUES (0000001, 5, 00001, 0004),
+(0000002, 5, 00001, 0005);
+
+/* Creating Ships relationships */
+INSERT INTO Ships
+VALUES (00001, 0000001),
+(00001, 0000002);
+
+/* Creating Contains relationships */
+INSERT INTO Contains
+VALUES (0004, 0000001),
+(0005, 0000002);
 
 /* Getting rid of a supplier */
 DELETE FROM Supplier
 WHERE SupplierID=00002;
+
+/* Rehiring the supplier */
+INSERT INTO Supplier
+VALUES (00002, 'Wings R Us', '4 Grey Road', 'Pheonix', 'AZ', 85010);
+
+
+
+
+
+
+
 
 
 
